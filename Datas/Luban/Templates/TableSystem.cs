59be20bccea9212f5411a4b9de2fdf0773dc5d95
ljp <2540297235@qq>
2025-05-28 11:29:46 +0800

    1111

---
+++ Datas/Luban/Templates/TableSystem.cs
@@ -0,0 +1,22 @@
+using cfg;
+using System.Collections.Generic;
+
+public static class TableSystem
+{
+    private static Dictionary<string, IVOFun> tables = new Dictionary<string, IVOFun>();
+
+    public static T GetVOData<T>() where T : IVOFun,new()
+    {
+        if (tables.ContainsKey(typeof(T).Name))
+        {
+            return (T)tables[typeof(T).Name];
+        }
+        else
+        {
+            var table = new T();
+            table._LoadData();
+            tables.Add(typeof(T).Name, table);
+            return (T)table;
+        }
+    }
+}
--- Datas/gen.bat
+++ Datas/gen.bat
@@ -1,14 +1,17 @@
 set WORKSPACE=..
 set CONF_ROOT=.
 set LUBAN_DLL=%CONF_ROOT%\Luban\Luban.dll
+set DATA_OUTPATH=%WORKSPACE%\Assets\Bundle\Config\TableData\json
+set CODE_OUTPATH=%WORKSPACE%\Assets\Client\Client_Logic\TableSystem\Tables
 
+xcopy /s /e /i /y "%CONF_ROOT%\Luban\Templates\TableSystem.cs" "%CODE_OUTPATH%\..\TableSystem.cs"
 
 dotnet %LUBAN_DLL% ^
     -t all ^
     -c cs-simple-json ^
     -d json ^
     --conf %CONF_ROOT%\luban.conf ^
-    -x outputCodeDir=%WORKSPACE%\Assets\Client\Client_Base\TableSystem\Tables ^
-    -x outputDataDir=%WORKSPACE%\Assets\Bundle\TableData
+    -x outputCodeDir=%CODE_OUTPATH% ^
+    -x outputDataDir=%DATA_OUTPATH%
 
 pause
