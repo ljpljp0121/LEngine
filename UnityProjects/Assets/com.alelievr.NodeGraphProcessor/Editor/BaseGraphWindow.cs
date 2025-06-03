9c60d5308aded68d0ff24491c9556f1f93e45329
ljp <2540297235@qq>
2025-06-03 18:34:43 +0800

    11

--- UnityProjects/Assets/com.alelievr.NodeGraphProcessor/Editor/BaseGraphWindow.cs
+++ UnityProjects/Assets/com.alelievr.NodeGraphProcessor/Editor/BaseGraphWindow.cs
@@ -9,14 +9,17 @@ using UnityEditor.Experimental.GraphView;
 
 namespace GraphProcessor
 {
+    /// <summary>
+    /// 图编辑器窗口的基类
+    /// </summary>
     [System.Serializable]
     public abstract class BaseGraphWindow : EditorWindow
     {
         protected VisualElement rootView;
-        protected BaseGraphView graphView;
+        protected BaseGraphView graphView; //图的编辑器视图
 
         [SerializeField]
-        protected BaseGraph graph;
+        protected BaseGraph graph; //图的运行时数据
 
         readonly string graphWindowStyle = "GraphProcessorStyles/BaseGraphView";
 
@@ -30,8 +33,8 @@ namespace GraphProcessor
 
         bool reloadWorkaround = false;
 
-        public event Action<BaseGraph> graphLoaded;
-        public event Action<BaseGraph> graphUnloaded;
+        public event Action<BaseGraph> graphLoaded;     //图加载回调
+        public event Action<BaseGraph> graphUnloaded;   //图卸载回调
 
         /// <summary>
         /// 窗口启用时调用
@@ -84,6 +87,7 @@ namespace GraphProcessor
 
         public void InitializeGraph(BaseGraph graph)
         {
+            //保存旧图
             if (this.graph != null && graph != this.graph)
             {
                 // Save the graph to the disk
@@ -99,19 +103,22 @@ namespace GraphProcessor
             if (graphView != null)
                 rootView.Remove(graphView);
 
-            //Initialize will provide the BaseGraphView
+            //初始化BaseGraphView
             InitializeWindow(graph);
-
             if (graphView == null)
             {
                 Debug.LogError("GraphView has not been added to the BaseGraph root view !");
                 return;
             }
             graphView.styleSheets.Add(Resources.Load<StyleSheet>(graphWindowStyle));
+            GridBackground gridBackground = new GridBackground();
+            gridBackground.StretchToParentSize();
+            graphView.Insert(0, gridBackground);
             graphView.Initialize(graph);
 
             InitializeGraphView(graphView);
 
+            //链接场景到BaseGraph
             if (graph.IsLinkedToScene())
                 LinkGraphWindowToScene(graph.GetLinkedScene());
             else
@@ -140,7 +147,14 @@ namespace GraphProcessor
             graphView = null;
         }
 
+        /// <summary>
+        /// 初始化BaseGraphWindow视图,必须要生成一个graphView
+        /// </summary>
         protected abstract void InitializeWindow(BaseGraph graph);
+        /// <summary>
+        /// 初始化graphView
+        /// </summary>
+        /// <param name="view"></param>
         protected virtual void InitializeGraphView(BaseGraphView view) { }
     }
 }
